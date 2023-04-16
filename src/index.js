b20ff9e8d1d330f57fc290eed07f72c73bdf9ab1
Jory Roberts <robertsjory13@gmail.com>
2023-04-16 15:04:27 -0500

    solution final refactor

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   addingEventListeners();
 });
 
+let taskObjArr = [];
 //get the form and add event listener
 const addingEventListeners = () => {
   document
@@ -18,7 +19,14 @@ const handleFormSubmit = (e) => {
   const task = e.target[0].value;
   const priorityLevel = parseInt(e.target.priority.value);
 
+  const taskObj = { task, priorityLevel };
+  taskObjArr.push(taskObj);
+
+  console.log(taskObjArr);
+
   displayTask(task, priorityLevel);
+  
+  
 };
 
 const displayTask = (task, priorityLevel) => {
@@ -35,6 +43,8 @@ const displayTask = (task, priorityLevel) => {
   taskLi.appendChild(deleteBtn);
 };
 
+
+
 const deleteTask = (e) => e.target.parentNode.remove();
 
 const getPriorityColor = (priorityLevel) => {
@@ -45,100 +55,14 @@ const getPriorityColor = (priorityLevel) => {
   } else if (priorityLevel === 3) return "green";
 };
 
-// function sortTasks() {
+// const sortTasks = () => {
 //   const sortTasksSelect = document.getElementById("sort-tasks");
 //   if (sortTasksSelect.value === "h-l") {
-//     taskArr.sort((a, b) => a.priorityLevel - b.priorityLevel);
+//     taskObjArr.sort((a, b) => a.priorityLevel - b.priorityLevel);
 //   } else {
-//     taskArr.sort((a, b) => b.priorityLevel - a.priorityLevel);
+//     taskObjArr.sort((a, b) => b.priorityLevel - a.priorityLevel);
 //   }
+  
 //   displayTasks();
-// }
-
-// newTaskForm.addEventListener("click", function (e) {
-//   if (e.target.dataset.action === "delete") {
-//     e.target.parentElement.remove();
-//   }
-// });
-
-//submit on form to add item to list
-// newTaskForm.addEventListener("submit", function (event) {
-//   // suppressing event to keep form from reloading page
-//   event.preventDefault();
-
-//   //query selector by Id
-//   const newTask = document.querySelector("#new-task-description").value;
-
-//   //updating tasklist with innerHTML method
-//   newTaskDescription.innerHTML += `<li> ${newTask}
-//   <button data-action = "delete" > Remove </button>
-//   </li>`;
-
-//   //Reset textfield to blank space to add next item to list
-//   taskForm.reset();
-// });
-
-//Click event that deletes task from list when x btn is clicked
-// newTaskForm.addEventListener("click", function (e) {
-//   if (e.target.dataset.action === "delete") {
-//     e.target.parentElement.remove();
-//   }
-// });
-
-//Input Id's
-//   const newTaskForm = document.getElementById("create-task-form");
-//   const newTaskDescription = document.getElementById("new-task-description");
-
-//   const newTaskUl = document.getElementById("tasks");
-
-//   //attach event listeners to DOM contents
-//   newTaskForm.addEventListener("submit", () => {
-//     createNewTask(event, newTaskDescription);
-//     //clear input field
-//     newTaskForm.reset();
-// });
-// });
-
-// const createNewTask = (event, newTaskDescription) => {
-//   //prevent form from refreshing when submit event occurs
-//   event.preventDefault();
-//   //create new li element where tasks will live
-//   const newTask = document.createElement("li");
-//   newTask.innerText = newTaskDescription.value;
-
-//   //new task appended to li
-//   appendNewTask(newTask);
-
-//   //create delete button
-//   const deleteButton = document.createElement("button");
-//   deleteButton.innerText = "Delete";
-
-//   deleteButton.addEventListener("click", (event) => {
-//     //delete task from li
-//     event.target.parentNode.remove();
-//   });
-
-//   newTask.appendChild(deleteButton);
 // };
 
-// const appendNewTask = (task) => {
-//   document.getElementById("tasks").appendChild(task);
-// };
-
-// //Generate Random Color
-
-// const random = (number) => {
-//   return Math.floor(Math.random() * (number * 1));
-// };
-
-// const changeColorButton = document.createElement("button");
-// changeColorButton.textContent = "Change Color";
-// changeColorButton.classList = "color-change";
-
-// changeColorButton.addEventListener("click", () => {
-//   const randomColor = `rgb(${random(255)}, ${random(255)}, ${random(255)}`;
-
-//   changeColorButton.style.backgroundColor = randomColor;
-// });
-
-// document.getElementById("main-content").appendChild(changeColorButton);
