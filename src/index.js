b302d4ca2631e845068929b27acef1b874fbd385
Jory Roberts <robertsjory13@gmail.com>
2023-04-16 14:00:10 -0500

    solution adds color change

--- src/index.js
+++ src/index.js
@@ -16,11 +16,12 @@ const addingEventListeners = () => {
 const handleFormSubmit = (e) => {
   e.preventDefault();
   const task = e.target[0].value;
+  const priorityLevel = parseInt(e.target.priority.value);
 
-  displayTask(task);
+  displayTask(task, priorityLevel);
 };
 
-const displayTask = (task) => {
+const displayTask = (task, priorityLevel) => {
   const taskUl = document.getElementById("tasks");
   const taskLi = document.createElement("li");
   const deleteBtn = document.createElement("button");
@@ -29,13 +30,20 @@ const displayTask = (task) => {
   deleteBtn.addEventListener("click", deleteTask);
 
   taskLi.textContent = task + " ";
+  taskLi.style.color = getPriorityColor(priorityLevel);
   taskUl.appendChild(taskLi);
   taskLi.appendChild(deleteBtn);
 };
 
 const deleteTask = (e) => e.target.parentNode.remove();
 
-
+const getPriorityColor = (priorityLevel) => {
+  if (priorityLevel === 1) {
+    return "red";
+  } else if (priorityLevel === 2) {
+    return "blue";
+  } else if (priorityLevel === 3) return "green";
+};
 
 // function sortTasks() {
 //   const sortTasksSelect = document.getElementById("sort-tasks");
